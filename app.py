966ecafa72e509537be5208c4e04abd666d2a622
Brian Wong <brian.wong@coherent.com.hk>
2020-03-02 16:33:25 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,107 @@
+import os
+from flask import Flask, render_template, request, jsonify
+#from firebase_admin import credentials, firestore, initialize_app
+from firebase import firebase
+# import json
+from datetime import date
+from twilio.rest import Client
+
+#import config
+#client = Client(config.ACCOUNT_SID, config.AUTH_TOKEN) 
+client = Client(ACCOUNT_SID, AUTH_TOKEN) 
+
+app = Flask(__name__)
+firebase = firebase.FirebaseApplication('https://fundpricescraperncb.firebaseio.com/') # copy this from Firebase >> Database >> link next to the chain icon
+
+# cred = credentials.Certificate('height-collector-firebase-adminsdk-4mjmy-8b0f39331c.json')
+# default_app = initialize_app(cred)
+# db = firestore.client()
+# todo_ref = db.collection('todos')
+
+def send_message(): 
+    result = firebase.get('/Data', name='') # name = '' gets all the data
+    
+    id_last = sorted(result.keys())[-1]
+    date_last = result[id_last]['Date']
+    price_last = result[id_last]['Price']
+
+    id_last_2 = sorted(result.keys())[-2]
+    date_last_2 = result[id_last_2]['Date']
+    price_last_2 = result[id_last_2]['Price']
+
+    change_pct = (float(price_last) - float(price_last_2)) / float(price_last_2)
+    
+    if change_pct >= 0:
+        pct_sign = '+'
+    else:
+        pct_sign = ''
+
+    message_body = '南商中國源動力基金：\n{0} 價格: {1}\n{2} 價格: {3}\n變動: {4}{5:.2%}'.format(date_last_2, price_last_2,
+                                                                                                                            date_last, price_last,
+                                                                                                                            pct_sign, change_pct)
+    # message = client.messages.create( 
+    #                             from_='whatsapp:+14155238886',  
+    #                             body= message_body,      
+    #                             to='whatsapp:+85262166578' 
+    #                         ) 
+    # message = client.messages.create( 
+    #                             from_='whatsapp:+14155238886',  
+    #                             body= message_body,      
+    #                             to='whatsapp:+85294362320' 
+    #                         ) 
+    
+    # print(message.sid)
+    return message_body
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        date_string = date.today().strftime('%Y-%m-%d')
+        price = request.form['price_name']
+        data = {
+            'Date': date_string,
+            'Price': price
+        }
+        result = firebase.post('/Data', data) # fundpricescraperncb comes from Firebase >> Database >> top of the tree
+
+        # SEND WHATSAPP MESSAGE
+        message = send_message()
+        
+        return render_template('success.html', message=message)
+    else:
+       pass
+
+# @app.route("/listdata")
+# def listdata():
+#     result = firebase.get('/height-collector/Data', name='') # name = '' gets all the data
+#     # result = {
+#     #     'Name': 'Testing'
+#     # }
+#     print(result)
+#     result2 = 'hello'
+#     return render_template('listdata.html', context=result)
+
+# @app.route("/updatedata")
+# def updatedata():
+#     result = firebase.get('/height-collector/Data', name='')
+#     id = str(sorted(result.keys())[0])
+#     email = result[id]['Email']
+#     height = 314
+#     firebase.put('/height-collector/Data/' + id, 'Height', height)
+#     return render_template('updatedata.html', email=email, height=height)
+
+# @app.route("/deletedata")
+# def deletedata():
+#     result = firebase.get('/height-collector/Data', name='')
+#     id = sorted(result.keys())[-1]
+#     email = result[id]['Email']
+#     firebase.delete('/height-collector/Data/', id)
+#     return render_template('deletedata.html', email=email)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
